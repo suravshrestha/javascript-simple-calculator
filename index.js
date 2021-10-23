49fb4e7019682a7a8aed6a85ad0deab4cd41888b
sks6174 <surav_02@live.com>
2021-10-23 16:32:00 +0545

    Add parenthesis button functionality

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const clearBtn = document.getElementById("clear");
 const backspaceBtn = document.getElementById("backspace");
 
 const equalsBtn = document.getElementById("equals");
+const parenthesisBtn = document.getElementById("parenthesis");
 
 let equalsClicked = 0;
 let operatorClicked = 0;
+let openingParenthesisClicked = 0;
 
 function evaluateExpression() {
   const expression = expInputEl.textContent
@@ -35,10 +37,16 @@ clearBtn.addEventListener("click", () => {
 
   equalsClicked = 0;
   operatorClicked = 0;
+  openingParenthesisClicked = 0;
 });
 
 backspaceBtn.addEventListener("click", () => {
   const span = expInputEl.lastElementChild;
+  const prevText = span.textContent;
+
+  if (/ [+−×/] /.test(prevText) || prevText === " × (") {
+    operatorClicked = 0;
+  }
 
   if (span) {
     expInputEl.removeChild(span);
@@ -56,6 +64,7 @@ numBtns.forEach((numBtn) => {
 
     equalsClicked = 0;
     operatorClicked = 0;
+    openingParenthesisClicked = 0;
 
     const span = document.createElement("span");
     span.textContent = e.target.textContent;
@@ -73,8 +82,12 @@ operatorBtns.forEach((operatorBtn) => {
     }
 
     const prevOperator = expInputEl.lastChild.textContent;
-    if (operatorClicked && prevOperator !== "%") {
-      // Repeating operators (except for %)
+    if (
+      (operatorClicked && prevOperator !== "%") ||
+      prevOperator === "(" ||
+      prevOperator === " × ("
+    ) {
+      // Repeating operators (except for %) or operator clicked after opening parenthesis
       return;
     }
 
@@ -85,6 +98,7 @@ operatorBtns.forEach((operatorBtn) => {
 
     operatorClicked = e.target.id === "percent" ? 0 : 1; // Allow operation if previous operator = %
     equalsClicked = 0;
+    openingParenthesisClicked = 0;
 
     const span = document.createElement("span");
 
@@ -123,4 +137,34 @@ equalsBtn.addEventListener("click", () => {
   expInputEl.appendChild(span);
 
   output.textContent = "";
+});
+
+parenthesisBtn.addEventListener("click", () => {
+  const prevEl = expInputEl.lastChild;
+  const prevText = prevEl ? prevEl.textContent : null;
+
+  const openingParenthesisCount = (expInputEl.textContent.match(/[(]/g) || [])
+    .length;
+  const closingParenthesisCount = (expInputEl.textContent.match(/[)]/g) || [])
+    .length;
+  const unclosedParenthesisCount =
+    openingParenthesisCount - closingParenthesisCount;
+
+  const span = document.createElement("span");
+  span.style.color = "#fafafa";
+
+  if (prevText === "(" || prevText === " × (" || / [+−×/] /.test(prevText)) {
+    span.textContent = "(";
+    openingParenthesisClicked = 1;
+  } else if (unclosedParenthesisCount >= 1) {
+    span.textContent = ")";
+  } else if (/[0-9]/.test(prevText) || prevText === ")") {
+    span.innerHTML = `<span style="color:#94fc13"> &times;</span><span> (</span>`;
+    openingParenthesisClicked = 1;
+  } else {
+    span.textContent = "(";
+    openingParenthesisClicked = 1;
+  }
+
+  expInputEl.appendChild(span);
 });
