88f8bd0c3dd6bb6f5dfcecc6a17e39a4e72128b0
sks6174 <surav_02@live.com>
2021-10-24 20:21:58 +0545

    Remove precision

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ function evaluateExpression() {
   try {
     // For handling floating decimals
     // Show at most 10 decimals if necessary
-    output.textContent =
-      Math.round((eval(expression) + Number.EPSILON) * 10 ** 10) / 10 ** 10;
+    // output.textContent = Math.round((eval(expression) + Number.EPSILON) * 10 ** 10) / 10 ** 10;
+    output.textContent = eval(expression);
   } catch (error) {
     output.textContent = "";
   }
