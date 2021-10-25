b19fa7827ebb1f3bf93837931331d2cfed83b08d
sks6174 <surav_02@live.com>
2021-10-25 18:39:00 +0545

    Replace DOMNodeInserted with MutationObserver

--- index.js
+++ index.js
@@ -49,8 +49,26 @@ function unclosedParenthesisCount() {
   return unclosed;
 }
 
-// Detect number and operator insertion (button click)
-expInputEl.addEventListener("DOMNodeInserted", evaluateExpression);
+// Detect changes to the input element (button click)
+// Options for the observer (which mutations to observe)
+const config = { childList: true };
+
+// Callback function to execute when mutations are observed
+function callback(mutationsList, observer) {
+  // Use traditional 'for loops' for IE 11
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      // A chlid node has been added or removed
+      evaluateExpression();
+    }
+  }
+}
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(expInputEl, config);
 
 clearBtn.addEventListener("click", () => {
   expInputEl.textContent = "";
